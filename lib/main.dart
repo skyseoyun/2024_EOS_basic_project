4ed139705843857f10ef6734aefd56896e608de3
jooseoyun <skyseoyoon@naver.com>
2024-09-04 12:35:06 +0900

    assignment

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: '주서윤 앱입니다'),
+      home: const MyHomePage(title:' 주서윤 앱입니다'),
     );
   }
 }
